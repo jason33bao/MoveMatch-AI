c1792f16543c329299852c9f8fe83ef094ed4ebc
jasonbao <3036504207@qq.com>
2026-04-23 16:20:34 +0800

    Initial Commit

---
+++ MoveMatch AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoveMatch AI
+//
+//  Created by 包哲旭 on 23/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoveMatch AI/MoveMatch_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMatch_AIApp.swift
+//  MoveMatch AI
+//
+//  Created by 包哲旭 on 23/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoveMatch_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
